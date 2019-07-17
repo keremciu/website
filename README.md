cf925ad0975b29e109c8156b32b33c6791f18518
Kerem Sevencan <kerem@ritmix.org>
2019-07-17 16:05:04 +0200

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# Website
-
-It's my personal website which I tried to show my stuff.
-
-This project was bootstrapped with Create React App. You can get details about it from [Create React App](https://github.com/facebook/create-react-app).
---
+++ precache-manifest.eb73d9056d2e3b5d5ce628ca5282b269.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91352ac1231e78f9ce8357a4b8ea9ef8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a05f576fa9793935a356",
+    "url": "/static/css/main.520cb6a5.chunk.css"
+  },
+  {
+    "revision": "32c806e08fc33881c83f",
+    "url": "/static/js/2.5c4929e8.chunk.js"
+  },
+  {
+    "revision": "a05f576fa9793935a356",
+    "url": "/static/js/main.98833a23.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
