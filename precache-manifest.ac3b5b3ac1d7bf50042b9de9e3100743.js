3b3af17ffaf6c8ce768268e56c8666ab0ee46813
Kerem Sevencan <kerem@ritmix.org>
2019-11-01 14:06:05 +0100

    Updates

--- precache-manifest.ac3b5b3ac1d7bf50042b9de9e3100743.js
+++ precache-manifest.ac3b5b3ac1d7bf50042b9de9e3100743.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "812767b5ee7849143d266e9bae53edbf",
+    "revision": "74b0555865fb907c240c0225263ac7f8",
     "url": "/index.html"
   },
   {
-    "revision": "58fa425e394b0230804c",
+    "revision": "611d3d7489805ee8c997",
     "url": "/static/css/main.520cb6a5.chunk.css"
   },
   {
-    "revision": "32c806e08fc33881c83f",
-    "url": "/static/js/2.5c4929e8.chunk.js"
+    "revision": "615ee98f31dd2e33068c",
+    "url": "/static/js/2.2f997ac5.chunk.js"
   },
   {
-    "revision": "58fa425e394b0230804c",
-    "url": "/static/js/main.f882692a.chunk.js"
+    "revision": "611d3d7489805ee8c997",
+    "url": "/static/js/main.2f66cdcd.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.caedf3dd50111db7756e061151c27db8.js"
+  "/precache-manifest.ac3b5b3ac1d7bf50042b9de9e3100743.js"
 );
 
 self.addEventListener('message', (event) => {
