e3f14d7b294c1e246591d8ba72140010a3a19545
Kerem Sevencan <kerem@ritmix.org>
2021-02-16 23:41:43 +0100

    put hairstyle feature

--- components/intro.js
+++ components/intro.js
@@ -1,22 +1,42 @@
+import { useState } from 'react'
+
 import Hero from "./hero";
 
+const hairList = ['', 'mohawk', 'gray', 'long']
+
 export default function Intro() {
+  const [reveal, setReveal] = useState(false)
+  const [hairStyle, setHairStyle] = useState('mohawk')
+  const filteredHairList = hairList.filter(style => style !== hairStyle)
+  const changeAvatar = () => {
+    setReveal(true)
+    setHairStyle(filteredHairList[Math.floor(Math.random() * filteredHairList.length)])
+  }
+
   return (
     <div className="container">
       <section>
-        <div className="avatar">
-          <picture>
+        <div className="avatar" onClick={changeAvatar}>
+          <picture className="top">
             <source
-              srcSet="/avatar_256.webp 1x, /avatar_384.webp 2x, /avatar_640.webp 3x"
+              srcSet="/avatar_256.webp 1x, /avatar_640.webp 2x, /avatar_828.webp 3x"
               type="image/webp"
             />
             <source srcSet="/avatar.png" type="image/png" />
             <img
               alt="Drawing of Kerem Sevencan"
-              src="/avatar_640.webp"
+              src="/avatar_828.webp"
               style={{ width: '12rem', height: '10.7rem' }}
             />
           </picture>
+          {hairStyle !== '' &&
+            <img
+              alt={`${hairStyle} of Kerem`}
+              src={`/${hairStyle}_hair.png`}
+              className={reveal ? 'bottom reveal' : 'bottom'}
+              onAnimationEnd={() => setReveal(false)}
+            />
+          }
         </div>
         <Hero
           label="DIGITAL EXPERIENCE DEVELOPER"
@@ -45,12 +65,45 @@ export default function Intro() {
       </section>
       <style jsx>{`
         .avatar {
+          position: relative;
           display: block;
           margin-bottom: 2rem;
+          height: 11rem;
+          cursor: pointer;
         }
-        .avatar img {
+        .avatar img.top {
+          position: absolute;
           animation-name: example;
           animation-duration: 0.5s;
+        }
+        .avatar img.bottom {
+          position: absolute;
+          z-index: 2;
+          width: 4.8rem;
+          height: 4.6rem;
+          left: 3.95rem;
+          top: -0.55rem;
+        }
+        .avatar img.bottom.reveal {
+          animation-name: reveal;
+          animation-duration: 0.8s;
+          animation-timing-function: ease-in-out;
+          transform: scale(1.2);
+        }
+        @keyframes reveal {
+          0% {
+            opacity: 0;
+            transform: scale(1.1);
+          }
+          30% {
+            transform: scale(1.1);
+          }
+          60% {
+            opacity: 1;
+          }
+          100% {
+            transform: scale(1);
+          }
         }
         @keyframes example {
           from {
